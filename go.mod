604b812cf6974465ed8332d0c593ef6cb1f613dc
Kamil Krzywicki <12999736+camaeel@users.noreply.github.com>
2025-11-04 08:13:04 +0100

    chore: bump golang and dependencies
    Signed-off-by: Kamil Krzywicki <12999736+camaeel@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/dbschenker/node-undertaker
 
-go 1.24.9
+go 1.25.3
 
 require (
 	github.com/aws/aws-sdk-go-v2/config v1.29.9
@@ -117,7 +117,7 @@ require (
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/shirou/gopsutil/v3 v3.24.5 // indirect
-	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/shoenig/go-m1cpu v0.1.7 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
 	github.com/spf13/cast v1.7.1 // indirect
--- pkg/nodeundertaker/nodeundertaker_test.go
+++ pkg/nodeundertaker/nodeundertaker_test.go
@@ -1,217 +1,217 @@
 package nodeundertaker
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"github.com/dbschenker/node-undertaker/cmd/node-undertaker/flags"
-	"github.com/dbschenker/node-undertaker/pkg/kubeclient"
-	"github.com/dbschenker/node-undertaker/pkg/nodeundertaker/config"
-	mock_observability "github.com/dbschenker/node-undertaker/pkg/observability/mocks"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
-	"github.com/stretchr/testify/assert"
-	"go.uber.org/mock/gomock"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/fake"
-	"testing"
-	"time"
+  "context"
+  "errors"
+  "fmt"
+  "github.com/dbschenker/node-undertaker/cmd/node-undertaker/flags"
+  "github.com/dbschenker/node-undertaker/pkg/kubeclient"
+  "github.com/dbschenker/node-undertaker/pkg/nodeundertaker/config"
+  mock_observability "github.com/dbschenker/node-undertaker/pkg/observability/mocks"
+  log "github.com/sirupsen/logrus"
+  "github.com/spf13/viper"
+  "github.com/stretchr/testify/assert"
+  "go.uber.org/mock/gomock"
+  "k8s.io/client-go/kubernetes"
+  "k8s.io/client-go/kubernetes/fake"
+  "testing"
+  "time"
 )
 
 func TestGetCloudProviderNoProvider(t *testing.T) {
-	ctx := context.TODO()
-	cfg := config.Config{}
-	cloudProvider, err := getCloudProvider(ctx, &cfg)
+  ctx := context.TODO()
+  cfg := config.Config{}
+  cloudProvider, err := getCloudProvider(ctx, &cfg)
 
-	assert.Nil(t, cloudProvider)
-	assert.Error(t, err)
+  assert.Nil(t, cloudProvider)
+  assert.Error(t, err)
 }
 
 func TestGetCloudProviderUnknownProvider(t *testing.T) {
-	ctx := context.TODO()
-	cfg := config.Config{}
-	viper.Set("cloud-provider", "unknown")
-	cloudProvider, err := getCloudProvider(ctx, &cfg)
+  ctx := context.TODO()
+  cfg := config.Config{}
+  viper.Set("cloud-provider", "unknown")
+  cloudProvider, err := getCloudProvider(ctx, &cfg)
 
-	assert.Nil(t, cloudProvider)
-	assert.Error(t, err)
+  assert.Nil(t, cloudProvider)
+  assert.Error(t, err)
 }
 
 func TestGetCloudProviderOk(t *testing.T) {
-	ctx := context.TODO()
-	cfg := config.Config{}
-	viper.Set("cloud-provider", "aws")
-	cloudProvider, err := getCloudProvider(ctx, &cfg)
+  ctx := context.TODO()
+  cfg := config.Config{}
+  viper.Set("cloud-provider", "aws")
+  cloudProvider, err := getCloudProvider(ctx, &cfg)
 
-	assert.NotNil(t, cloudProvider)
-	assert.NoError(t, err)
+  assert.NotNil(t, cloudProvider)
+  assert.NoError(t, err)
 }
 
 func TestGetCloudProviderKindOk(t *testing.T) {
-	ctx := context.TODO()
-	cfg := config.Config{}
-	viper.Set("cloud-provider", "kind")
-	cloudProvider, err := getCloudProvider(ctx, &cfg)
+  ctx := context.TODO()
+  cfg := config.Config{}
+  viper.Set("cloud-provider", "kind")
+  cloudProvider, err := getCloudProvider(ctx, &cfg)
 
-	assert.NotNil(t, cloudProvider)
-	assert.NoError(t, err)
+  assert.NotNil(t, cloudProvider)
+  assert.NoError(t, err)
 }
 
 func TestGetCloudProviderKwokOk(t *testing.T) {
-	ctx := context.TODO()
-	cfg := config.Config{}
-	viper.Set("cloud-provider", "kwok")
-	cloudProvider, err := getCloudProvider(ctx, &cfg)
+  ctx := context.TODO()
+  cfg := config.Config{}
+  viper.Set("cloud-provider", "kwok")
+  cloudProvider, err := getCloudProvider(ctx, &cfg)
 
-	assert.NotNil(t, cloudProvider)
-	assert.NoError(t, err)
+  assert.NotNil(t, cloudProvider)
+  assert.NoError(t, err)
 }
 
 func TestStartServerOk(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	errorMsg := "Error happened"
+  mockCtrl := gomock.NewController(t)
+  defer mockCtrl.Finish()
+  const errorMsg = "Error happened"
 
-	observability := mock_observability.NewMockOBSERVABILITYSERVER(mockCtrl)
-	observability.EXPECT().StartServer(gomock.Any()).Times(1).DoAndReturn(
-		func(ctx3 context.Context) error {
-			select {
-			case <-ctx3.Done():
-				return fmt.Errorf(errorMsg)
-			case <-time.After(1 * time.Second):
-				return nil
-			}
-		})
+  observability := mock_observability.NewMockOBSERVABILITYSERVER(mockCtrl)
+  observability.EXPECT().StartServer(gomock.Any()).Times(1).DoAndReturn(
+    func(ctx3 context.Context) error {
+      select {
+      case <-ctx3.Done():
+        return fmt.Errorf(errorMsg)
+      case <-time.After(1 * time.Second):
+        return nil
+      }
+    })
 
-	ctx, cancel := context.WithCancel(context.TODO())
-	defer cancel()
-	cfg := config.Config{}
-	cfg.K8sClient = fake.NewClientset()
-	workload := func(ctx2 context.Context) error {
-		select {
-		case <-ctx2.Done():
-			return fmt.Errorf(errorMsg)
-		case <-time.After(5 * time.Second):
-			return nil
-		}
-	}
+  ctx, cancel := context.WithCancel(context.TODO())
+  defer cancel()
+  cfg := config.Config{}
+  cfg.K8sClient = fake.NewClientset()
+  workload := func(ctx2 context.Context) error {
+    select {
+    case <-ctx2.Done():
+      return fmt.Errorf(errorMsg)
+    case <-time.After(5 * time.Second):
+      return nil
+    }
+  }
 
-	res := startServer(ctx, &cfg, observability, workload, cancel)
-	assert.NoError(t, res)
+  res := startServer(ctx, &cfg, observability, workload, cancel)
+  assert.NoError(t, res)
 }
 
 func TestStartServerNok(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
+  mockCtrl := gomock.NewController(t)
+  defer mockCtrl.Finish()
 
-	errorMsg := "Error happened"
+  const errorMsg = "Error happened"
 
-	observability := mock_observability.NewMockOBSERVABILITYSERVER(mockCtrl)
-	observability.EXPECT().StartServer(gomock.Any()).Times(1).DoAndReturn(
-		func(ctx3 context.Context) error {
-			return fmt.Errorf(errorMsg)
-		})
+  observability := mock_observability.NewMockOBSERVABILITYSERVER(mockCtrl)
+  observability.EXPECT().StartServer(gomock.Any()).Times(1).DoAndReturn(
+    func(ctx3 context.Context) error {
+      return fmt.Errorf(errorMsg)
+    })
 
-	ctx, cancel := context.WithCancel(context.TODO())
-	defer cancel()
-	cfg := config.Config{}
-	cfg.K8sClient = fake.NewClientset()
+  ctx, cancel := context.WithCancel(context.TODO())
+  defer cancel()
+  cfg := config.Config{}
+  cfg.K8sClient = fake.NewClientset()
 
-	workload := func(ctx2 context.Context) error {
-		select {
-		case <-ctx2.Done():
-			return fmt.Errorf(errorMsg)
-		case <-time.After(5 * time.Second):
-			return nil
-		}
-	}
+  workload := func(ctx2 context.Context) error {
+    select {
+    case <-ctx2.Done():
+      return fmt.Errorf(errorMsg)
+    case <-time.After(5 * time.Second):
+      return nil
+    }
+  }
 
-	var res error
-	assert.NotPanics(t,
-		func() {
-			res = startServer(ctx, &cfg, observability, workload, cancel)
-		},
-	)
-	assert.EqualError(t, res, errorMsg)
+  var res error
+  assert.NotPanics(t,
+    func() {
+      res = startServer(ctx, &cfg, observability, workload, cancel)
+    },
+  )
+  assert.EqualError(t, res, errorMsg)
 }
 
 func TestCancelOnSigterm(t *testing.T) {
-	counter := 0
-	c := func() {
-		counter += 1
-	}
-	cancelOnSigterm(c)
-	assert.Equal(t, 0, counter)
+  counter := 0
+  c := func() {
+    counter += 1
+  }
+  cancelOnSigterm(c)
+  assert.Equal(t, 0, counter)
 }
 
 func TestSetupLogLevelNok(t *testing.T) {
-	err := setupLogging()
-	assert.Error(t, err)
+  err := setupLogging()
+  assert.Error(t, err)
 }
 
 func TestSetupLogFormatJsonOk(t *testing.T) {
-	originalLvl := log.GetLevel()
-	viper.Set(flags.LogLevelFlag, "error")
-	viper.Set(flags.LogFormatFlag, flags.LogFormatText)
-	err := setupLogging()
+  originalLvl := log.GetLevel()
+  viper.Set(flags.LogLevelFlag, "error")
+  viper.Set(flags.LogFormatFlag, flags.LogFormatText)
+  err := setupLogging()
 
-	assert.NoError(t, err)
-	assert.Equal(t, log.ErrorLevel, log.GetLevel())
-	//cleanup
-	log.SetLevel(originalLvl)
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp: true,
-	})
+  assert.NoError(t, err)
+  assert.Equal(t, log.ErrorLevel, log.GetLevel())
+  //cleanup
+  log.SetLevel(originalLvl)
+  log.SetFormatter(&log.TextFormatter{
+    FullTimestamp: true,
+  })
 }
 
 func TestSetupLogFormatNok(t *testing.T) {
-	originalLvl := log.GetLevel()
-	viper.Set(flags.LogLevelFlag, "error")
-	viper.Set(flags.LogFormatFlag, "unknonw")
-	err := setupLogging()
+  originalLvl := log.GetLevel()
+  viper.Set(flags.LogLevelFlag, "error")
+  viper.Set(flags.LogFormatFlag, "unknonw")
+  err := setupLogging()
 
-	assert.Error(t, err)
-	//cleanup
-	log.SetLevel(originalLvl)
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp: true,
-	})
+  assert.Error(t, err)
+  //cleanup
+  log.SetLevel(originalLvl)
+  log.SetFormatter(&log.TextFormatter{
+    FullTimestamp: true,
+  })
 }
 
 func TestSetupLogLevelOk(t *testing.T) {
-	originalLvl := log.GetLevel()
-	viper.Set(flags.LogLevelFlag, "error")
-	viper.Set(flags.LogFormatFlag, flags.LogFormatJson)
-	err := setupLogging()
+  originalLvl := log.GetLevel()
+  viper.Set(flags.LogLevelFlag, "error")
+  viper.Set(flags.LogFormatFlag, flags.LogFormatJson)
+  err := setupLogging()
 
-	assert.NoError(t, err)
-	assert.Equal(t, log.ErrorLevel, log.GetLevel())
-	//cleanup
-	log.SetLevel(originalLvl)
+  assert.NoError(t, err)
+  assert.Equal(t, log.ErrorLevel, log.GetLevel())
+  //cleanup
+  log.SetLevel(originalLvl)
 }
 
 func TestExecuteWithContext(t *testing.T) {
-	viper.Set(flags.LeaseLockNameFlag, "test-lease")
-	viper.Set(flags.PortFlag, 0) //use random port
-	viper.Set(flags.CloudProviderFlag, "kwok")
+  viper.Set(flags.LeaseLockNameFlag, "test-lease")
+  viper.Set(flags.PortFlag, 0) //use random port
+  viper.Set(flags.CloudProviderFlag, "kwok")
 
-	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
-	defer cancel()
+  ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
+  defer cancel()
 
-	err := executeWithContext(ctx, kubeclient.GetFakeClient, cancel)
-	assert.NoError(t, err)
+  err := executeWithContext(ctx, kubeclient.GetFakeClient, cancel)
+  assert.NoError(t, err)
 }
 
 func TestExecuteWithContextK8sErr(t *testing.T) {
-	viper.Set(flags.LeaseLockNameFlag, "test-lease")
-	viper.Set(flags.PortFlag, 0) //use random port
-	viper.Set(flags.CloudProviderFlag, "kwok")
+  viper.Set(flags.LeaseLockNameFlag, "test-lease")
+  viper.Set(flags.PortFlag, 0) //use random port
+  viper.Set(flags.CloudProviderFlag, "kwok")
 
-	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
-	defer cancel()
+  ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
+  defer cancel()
 
-	err := executeWithContext(ctx, func() (kubernetes.Interface, string, error) {
-		return nil, "", errors.New("test error")
-	}, cancel)
-	assert.Error(t, err)
+  err := executeWithContext(ctx, func() (kubernetes.Interface, string, error) {
+    return nil, "", errors.New("test error")
+  }, cancel)
+  assert.Error(t, err)
 }
